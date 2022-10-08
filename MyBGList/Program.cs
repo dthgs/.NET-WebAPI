e79c52b7a6608511fdec9eade8bd3b45e3d6772d
dthgs <daniel.thor.gunnlaugsson@gmail.com>
2022-10-08 13:07:09 +0200

    wip: add BoardGame model and controller

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,8 +1,9 @@
-// Executed at the start of the application to register and configure the required services and middlewares to handle the HTTP request & response pipeline
+using MyBGList;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Executed at the start of the application to register and configure the required services and middlewares to handle the HTTP request & response pipeline
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -31,6 +32,23 @@ app.UseAuthorization();
  /** Minimal API routes **/
 app.MapGet("/error", () => Results.Problem());
 app.MapGet("/error/test", () => { throw new Exception("test"); }); // Testing the DeveloperExceptionPageMiddleware
+app.MapGet("/BoardGames", () => new[] {
+    new BoardGame() {
+        Id = 1,
+        Name = "Axis & Allies",
+        Year = 1981
+    },
+    new BoardGame() {
+        Id = 2,
+        Name = "Citadels",
+        Year = 2000
+    },
+    new BoardGame() {
+        Id = 3,
+        Name = "Terraforming Mars",
+        Year = 2016
+    }
+});
 
 app.MapControllers();
 app.Run();
