e8ed6cfe1a9babb73dc2983ac037b87ba8d06910
dthgs <daniel.thor.gunnlaugsson@gmail.com>
2022-10-08 15:17:06 +0200

    chore: add CORS

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,15 +1,28 @@
+using Microsoft.AspNetCore.Cors;
 using MyBGList;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Executed at the start of the application to register and configure the required services and middlewares to handle the HTTP request & response pipeline
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(cfg => {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin",
+        cfg => {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(opts => 
-    opts.ResolveConflictingActions(apiDesc =>  apiDesc.First()) //  deal with routing conflict situations (not encouraged, keeping it for now)
+    opts.ResolveConflictingActions(apiDesc =>  apiDesc.First()) //  Deal with routing conflict situations (not encouraged, keeping it for now)
 );
 
 var app = builder.Build();
@@ -22,16 +35,19 @@ if (app.Configuration.GetValue<bool>("UseSwagger"))
 }
 
 if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
-    app.UseDeveloperExceptionPage(); // captures exceptions from the HTTP pipeline and generate an HTML error page
+    app.UseDeveloperExceptionPage(); // Captures exceptions from the HTTP pipeline and generate an HTML error page
 else
-    app.UseExceptionHandler("/error"); // sends relevant error info to a customizable handler
+    app.UseExceptionHandler("/error"); // Sends relevant error info to a customizable handler
 
 app.UseHttpsRedirection();
+
+app.UseCors();
+
 app.UseAuthorization();
 
- /** Minimal API routes **/
-app.MapGet("/error", () => Results.Problem());
-app.MapGet("/error/test", () => { throw new Exception("test"); }); // Testing the DeveloperExceptionPageMiddleware
+ /** Minimal API **/
+app.MapGet("/error", [EnableCors("AnyOrigin")] () => Results.Problem());
+app.MapGet("/error/test", [EnableCors("AnyOrigin")] () => { throw new Exception("test"); });
 
 app.MapControllers();
 
