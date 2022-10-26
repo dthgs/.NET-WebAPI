3f856d45219bb0f4dac16d4f69147f7cbbc26391
Daniel Thor Gunnlaugsson <daniel.thor.gunnlaugsson@gmail.com>
2022-10-26 15:45:40 +0200

    feat(boardgames): add paging

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -21,16 +21,25 @@ namespace MyBGList.Controllers
 
         [HttpGet(Name = "GetBoardGames")]
         [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
-        public async Task<RestDTO<BoardGame[]>> Get()
+        public async Task<RestDTO<BoardGame[]>> Get(int pageIndex = 0, int pageSize = 10)
         {
-            var query = _context.BoardGames; // Creating the IQueryable<T> expression tree
+            var query = _context.BoardGames
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize);
 
             return new RestDTO<BoardGame[]>()
             {
-                Data = await query.ToArrayAsync(), // Executing the IQueryable<T>
+                Data = await query.ToArrayAsync(),
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                RecordCount = await _context.BoardGames.CountAsync(),
                 Links = new List<LinkDTO> {
                     new LinkDTO(
-                        Url.Action(null, "BoardGames", null, Request.Scheme)!,
+                        Url.Action(
+                            null,
+                            "BoardGames",
+                            new { pageIndex, pageSize },
+                            Request.Scheme)!,
                         "self",
                         "GET"),
                 }
